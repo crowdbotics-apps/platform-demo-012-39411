30913764f111614fa1f7249cfe6c27ac7ce57462
shahraiz-cb <shahraiz.ali@crowdbotics.com>
2023-03-16 22:30:22 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/product-listing/index.js
+++ screens/product-listing/index.js
@@ -1,82 +1,59 @@
-import React, { useState, useEffect } from "react";
+// import { api_v1_recipe_list } from "./../../store/foodfindingsAPI/recipes.slice.js";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import React from "react";
 import { Text, View, StyleSheet, Image, Pressable, FlatList } from "react-native";
+const products = [{
+  id: 1,
+  title: "Dish name",
+  count: 1,
+  image: require("./assets/productImage.png")
+}, {
+  id: 2,
+  title: "Dish name",
+  count: 1,
+  image: require("./assets/productImage.png")
+}, {
+  id: 3,
+  title: "Dish name",
+  count: 1,
+  image: require("./assets/productImage.png")
+}, {
+  id: 4,
+  title: "Dish name",
+  count: 1,
+  image: require("./assets/productImage.png")
+}];
 
-const ProductListing = () => {
-  const [products, setProducts] = useState([]);
-  const [selectedProducts, setSelectedProducts] = useState([]);
-  useEffect(() => {
-    setProducts([{
-      id: 1,
-      title: "Dish name",
-      count: 1,
-      image: require("./assets/productImage.png")
-    }, {
-      id: 2,
-      title: "Dish name",
-      count: 1,
-      image: require("./assets/productImage.png")
-    }, {
-      id: 3,
-      title: "Dish name",
-      count: 1,
-      image: require("./assets/productImage.png")
-    }, {
-      id: 4,
-      title: "Dish name",
-      count: 1,
-      image: require("./assets/productImage.png")
-    }]);
-  }, []);
+const ProductListing = ({
+  navigation
+}) => {
+  const dispatch = useDispatch();
+  useEffect(() => {// dispatch(api_v1_recipe_list());
+  }, []); // use selector bug: upcoming fixes this sprint
 
-  const handleProductSelect = product => {
-    if (selectedProducts.includes(product)) {
-      const newSelectedProducts = selectedProducts.filter(selectedProduct => selectedProduct.id !== product.id);
-      setSelectedProducts(newSelectedProducts);
-    } else {
-      setSelectedProducts([...selectedProducts, product]);
-    }
-  };
-
-  const handleCountChange = (type, product) => {
-    const newProducts = products.map(item => {
-      if (item.id === product.id) {
-        if (type === "increase") {
-          item.count++;
-        } else if (type === "decrease") {
-          if (item.count > 1) {
-            item.count--;
-          }
-        }
-      }
-
-      return item;
-    });
-    setProducts(newProducts);
-  };
-
+  const {
+    entities: Recipes
+  } = useSelector(state => state.Recipes);
   return <View style={styles.container}>
-      <FlatList data={products} renderItem={({
+      <FlatList data={Recipes} renderItem={({
       item
     }) => <View style={styles.productContainer}>
             <View style={styles.imageContainer}>
-              <Image style={styles.image} source={item.image} />
-              <Radio style={styles.radio} selected={selectedProducts.includes(item)} onPress={() => handleProductSelect(item)} />
+              <Image style={styles.image} source={require("./Recipeful(2).png")} />
             </View>
             <View style={styles.flexRow}>
               <Text style={styles.productName}>{item.title}</Text>
               <View style={styles.flexRow}>
-                <Text style={styles.counterText}>
-                  {item.count} item{item.count > 1 && "s"}
-                </Text>
-                <Pressable onPress={() => handleCountChange("decrease", item)}>
-                  <Image style={styles.icon} source={require("./assets/decrementIcon.png")} />
-                </Pressable>
-                <Pressable onPress={() => handleCountChange("increase", item)}>
-                  <Image style={styles.icon} source={require("./assets/incrementIcon.png")} />
-                </Pressable>
+                <Text style={styles.counterText}>{item.rating}</Text>
               </View>
             </View>
-            <Button buttonText="Order" style={styles.button} hideShadow />
+            <Pressable onPress={() => {
+        navigation.navigate("aboutTheApp", {
+          item: item
+        });
+      }}><View style={styles.qeoNNhmC}><Text style={styles.TiVkDKYZ}>Details</Text></View></Pressable>
           </View>} />
     </View>;
 };
@@ -95,7 +72,7 @@ const styles = StyleSheet.create({
     marginVertical: 10
   },
   imageContainer: {
-    width: 350,
+    width: "100%",
     height: 150,
     borderRadius: 10,
     justifyContent: "center",
@@ -108,7 +85,9 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5
+    elevation: 5,
+    borderColor: "#E45700",
+    borderWidth: 1
   },
   radio: {
     position: "absolute",
@@ -144,89 +123,16 @@ const styles = StyleSheet.create({
   button: {
     marginHorizontal: 20,
     marginVertical: 15
+  },
+  qeoNNhmC: {
+    backgroundColor: "#E45700",
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 5
+  },
+  TiVkDKYZ: {
+    textAlign: "center",
+    color: "white"
   }
 });
 export default ProductListing;
-
-const Button = params => {
-  const backgroundColor = params.backgroundColor || "red";
-  const textColor = params.textColor || "#fff";
-  const btnStyle = {
-    backgroundColor: backgroundColor,
-    borderColor: params.borderColor || backgroundColor,
-    borderWidth: 1
-  };
-  const btnText = {
-    color: textColor
-  };
-  return <View style={[buttonStyles.btnContainer, params.style]}>
-      <View style={!params.hideShadow ? buttonStyles.shadowContainer : null}>
-        <Pressable style={[buttonStyles.btn, btnStyle]} onPress={params.onPress}>
-          <Text style={[buttonStyles.btnText, btnText]}>
-            {params.buttonText}
-          </Text>
-          <View style={styles.childrenContainer}>{params.children}</View>
-        </Pressable>
-      </View>
-    </View>;
-};
-
-const buttonStyles = StyleSheet.create({
-  btnContainer: {
-    justifyContent: "center"
-  },
-  shadowContainer: {
-    shadowColor: "rgba(0, 0, 0, 0.5)",
-    shadowOffset: {
-      width: 0,
-      height: 5
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 10,
-    elevation: 10,
-    backgroundColor: "#fff",
-    borderRadius: 10
-  },
-  btn: {
-    height: 50,
-    padding: 10,
-    paddingHorizontal: 25,
-    borderRadius: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "row"
-  },
-  btnText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold"
-  },
-  childrenContainer: {
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
-
-const Radio = props => {
-  return <Pressable style={[radioStyles.container, props.style]} onPress={props.onPress}>
-      {props.selected && <View style={radioStyles.radio} />}
-    </Pressable>;
-};
-
-const radioStyles = StyleSheet.create({
-  container: {
-    width: 20,
-    height: 20,
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: "#000",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  radio: {
-    width: 10,
-    height: 10,
-    borderRadius: 5,
-    backgroundColor: "#000"
-  }
-});
