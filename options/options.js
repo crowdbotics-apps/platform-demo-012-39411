7a638e25dcf4bd44b1726d263afa47ec7f25faf8
shahraiz-cb <shahraiz.ali@crowdbotics.com>
2023-04-04 20:28:30 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "platform-demo-012-39411",
-  url: "https://platform-demo-012-39411.botics.co",
-  api: "https://platform-demo-012-39411.botics.co/api/v1"
+  "name": "platform-demo-012-39411",
+  "url": "https://platform-demo-012-39411.botics.co",
+  "api": "https://platform-demo-012-39411.botics.co/api/v1",
+  "initialRoute": "Untitled1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
